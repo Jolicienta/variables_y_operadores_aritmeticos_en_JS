8e242b15818d40593f3efbd21436022d38146973
jolicienta <jolicienta.rp@gmail.com>
2022-05-01 23:45:13 -0400

    pasando de grados C a K y F

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+/* Como el desafío requería de una parte del script linkeado de forma externa, procedí a dejar
+el extra que no se pedía en el desafío de calcular el promedio total del curso */
+
+var promedio_total = (total_HTML + total_CSS + total_JS) / 3;
+promedioTOTAL = document.getElementById('promedioTOTAL').innerText = promedio_total;
